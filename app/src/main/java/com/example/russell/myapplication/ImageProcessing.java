865bf064353d11514b380ea5c9f5c702dc154ffb
Russell <sell_nat@yahoo.com>
2017-05-13 15:33:18 -0700

    Starting on tess ocr

---
+++ app/src/main/java/com/example/russell/myapplication/ImageProcessing.java
@@ -0,0 +1,207 @@
+package com.example.russell.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ImageProcessing extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView userImg;
+    private TextView textView;
+    private Button takePic;
+    String result = "empty";
+    private static final String lang = "eng";
+
+    private TessBaseAPI tessBaseApi;
+    private static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/TesseractSample/";
+    private static final String TESSDATA = "tessdata";
+    Uri outputFileUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_processing);
+        takePic = (Button) findViewById(R.id.TakePic);
+        textView = (TextView) findViewById(R.id.ImageText);
+        userImg = (ImageView) findViewById(R.id.UserImage);
+
+        takePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+
+
+
+
+    private void dispatchTakePictureIntent() {
+        try {
+            String IMGS_PATH = Environment.getExternalStorageDirectory().toString() + "/TesseractSample/imgs";
+            prepareDirectory(IMGS_PATH);
+
+            String img_path = IMGS_PATH + "/ocr.jpg";
+
+            outputFileUri = Uri.fromFile(new File(img_path));
+
+            final Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+
+
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    /**
+     * Prepare directory on external storage
+     *
+     * @param path
+     * @throws Exception
+     */
+    private void prepareDirectory(String path) {
+
+        File dir = new File(path);
+        if (!dir.exists()) {
+            if (!dir.mkdirs()) {
+
+            }
+        } else {
+
+        }
+    }
+
+    private void doOCR() {
+        prepareTesseract();
+        startOCR(outputFileUri);
+    }
+
+    private void prepareTesseract() {
+        try {
+            prepareDirectory(DATA_PATH + TESSDATA);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        copyTessDataFiles(TESSDATA);
+    }
+    /**
+     * Copy tessdata files (located on assets/tessdata) to destination directory
+     *
+     * @param path - name of directory with .traineddata files
+     */
+    private void copyTessDataFiles(String path) {
+        try {
+            String fileList[] = getAssets().list(path);
+
+            for (String fileName : fileList) {
+
+                // open file within the assets folder
+                // if it is not already there copy it to the sdcard
+                String pathToDataFile = DATA_PATH + path + "/" + fileName;
+                if (!(new File(pathToDataFile)).exists()) {
+
+                    InputStream in = getAssets().open(path + "/" + fileName);
+
+                    OutputStream out = new FileOutputStream(pathToDataFile);
+
+                    // Transfer bytes from in to out
+                    byte[] buf = new byte[1024];
+                    int len;
+
+                    while ((len = in.read(buf)) > 0) {
+                        out.write(buf, 0, len);
+                    }
+                    in.close();
+                    out.close();
+                }
+            }
+        } catch (IOException e) {
+        }
+    }
+
+    /**
+     * don't run this code in main thread - it stops UI thread. Create AsyncTask instead.
+     * http://developer.android.com/intl/ru/reference/android/os/AsyncTask.html
+     *
+     * @param imgUri
+     */
+    private void startOCR(Uri imgUri) {
+        try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 4; // 1 - means max size. 4 - means maxsize/4 size. Don't use value <4, because you need more memory in the heap to store your data.
+            Bitmap bitmap = BitmapFactory.decodeFile(imgUri.getPath(), options);
+
+            result = extractText(bitmap);
+
+            textView.setText(result);
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    private String extractText(Bitmap bitmap) {
+        try {
+            tessBaseApi = new TessBaseAPI();
+        } catch (Exception e) {
+
+            if (tessBaseApi == null) {
+
+            }
+        }
+
+        tessBaseApi.init(DATA_PATH, lang);
+
+//       //EXTRA SETTINGS
+//        //For example if we only want to detect numbers
+//        tessBaseApi.setVariable(TessBaseAPI.VAR_CHAR_WHITELIST, "1234567890");
+//
+//        //blackList Example
+//        tessBaseApi.setVariable(TessBaseAPI.VAR_CHAR_BLACKLIST, "!@#$%^&*()_+=-qwertyuiop[]}{POIU" +
+//                "YTRWQasdASDfghFGHjklJKLl;L:'\"\\|~`xcvXCVbnmBNM,./<>?");
+
+        tessBaseApi.setImage(bitmap);
+        String extractedText = "empty result";
+        try {
+            extractedText = tessBaseApi.getUTF8Text();
+        } catch (Exception e) {
+        }
+        tessBaseApi.end();
+        return extractedText;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //making photo
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            doOCR();
+        } else {
+            Toast.makeText(this, "ERROR: Image was not obtained.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/com/example/russell/myapplication/MainActivity.java
+++ app/src/main/java/com/example/russell/myapplication/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.russell.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button goToProcess;
+    private Intent goToProcessIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToProcessIntent = new Intent(getApplicationContext(),ImageProcessing.class);
+        goToProcess = (Button) findViewById(R.id.GoProcess);
+        goToProcess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(goToProcessIntent);
+            }
+        });
+
     }
 }
